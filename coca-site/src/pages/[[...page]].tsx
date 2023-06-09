a0e3173881cbc119445b8a4c7773007e53fc8c92
tiluuta <tiluutan@gmail.com>
2023-06-09 11:52:00 -0700

    Update

--- coca-site/src/pages/[[...page]].tsx
+++ coca-site/src/pages/[[...page]].tsx
@@ -4,22 +4,44 @@ import { BuilderComponent, builder, useIsPreviewing } from '@builder.io/react';
 import DefaultErrorPage from 'next/error';
 import Head from 'next/head';
 
+import { Builder } from '@builder.io/react';
+import dynamic from "next/dynamic";
+
 // Replace with your Public API Key
 builder.init('63508ae4144c457e90c900e821dea7b1');
 
 export async function getStaticProps({ params }) {
   // Fetch the builder content
+  const urlPath = '/' + (params?.page?.join('/') || '');
   const page = await builder
     .get('page', {
       userAttributes: {
-        urlPath: '/' + (params?.page?.join('/') || ''),
+        urlPath
       },
+    })
+    .toPromise();
+
+  const header = await builder
+    .get('header', { 
+      userAttributes: { 
+        urlPath
+      } 
+    })
+    .toPromise();
+
+  const footer = await builder
+    .get('footer', { 
+      userAttributes: { 
+        urlPath
+      } 
     })
     .toPromise();
 
   return {
     props: {
       page: page || null,
+      header: header || null,
+      footer: footer || null
     },
     revalidate: 5
   };
@@ -39,7 +61,7 @@ export async function getStaticPaths() {
   };
 }
 
-export default function Page({ page }) {
+export default function Page({ page, header }) {
   const router = useRouter();
   const isPreviewing = useIsPreviewing();
 
@@ -57,7 +79,9 @@ export default function Page({ page }) {
         <title>{page?.data.title}</title>
       </Head>
       {/* Render the Builder page */}
+      <BuilderComponent model="header" content={header} />
       <BuilderComponent model="page" content={page} />
+      <BuilderComponent model="footer" content={footer} />
     </>
   );
 }
